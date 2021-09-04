17988fd82d3f52f93e331a2225232e1ff70637b6
Harshavardhan Malve <>
2021-09-04 18:27:42 +0530

    major changes in aws-chime-web-server server.js

--- aws-chime-web-server/server.js
+++ aws-chime-web-server/server.js
@@ -1,28 +1,40 @@
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const express = require('express');
-const path = require('path');
-const hostname = 'localhost';
-const httpPort = 80;
-// const httpsPort = 443;
+'use strict'
 
-// const httpsOptions = {
-//     // cert: fs.readFileSync('www_esselworldtictactot_in.crt'),
-//     // ca: fs.readFileSync('www_esselworldtictactot_in.ca-bundle'),
-//     // key: fs.readFileSync('www.esselworldtictactot.in.key')
-//     cert: fs.readFileSync(path.join('/etc/letsencrypt/live/esselworldtictactot.in/cert.pem')),
-//     key: fs.readFileSync(path.join('/etc/letsencrypt/live/esselworldtictactot.in/privkey.pem'))
-// }
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
-const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(httpsOptions, app);
+const directoryToServe = 'server/frontend/';
+const httpsPort = 443;
+const httpPort = 80;
 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use('/static', express.static('files'));
 
-app.use(express.static('./files'));
-
-httpServer.listen(httpPort, hostname, () => {
-    console.log(`Listening on ${hostname}:${httpPort}`)
+app.get('/', function (req, res) {
+    console.log('path ' + path.join(__dirname, '..', directoryToServe));
+    res.sendFile(path.join(__dirname, '..', directoryToServe));
 });
-// httpsServer.listen(httpsPort, hostname);
+
+
+var httpsOptions = {
+    cert: fs.readFileSync(path.join('/etc/letsencrypt/live/harshmalve.com/cert.pem')),
+    key: fs.readFileSync(path.join('/etc/letsencrypt/live/harshmalve.com/privkey.pem'))
+};
+
+
+https.createServer(httpsOptions, app).listen(httpsPort, function () {
+    console.log('Magic happens on port ' + httpsPort);
+});
+
+// Redirect from http port to https
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'].replace(httpPort, httpsPort) + req.url });
+    console.log("http request, will go to >> ");
+    console.log("https://" + req.headers['host'].replace(httpPort, httpsPort) + req.url);
+    res.end();
+}).listen(httpPort);
