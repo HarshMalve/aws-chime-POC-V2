3b2c7ebc276f06fb6e57ce56279cbeb9ffb62273
Harshavardhan Malve <>
2021-09-06 13:18:30 +0530

    web server

--- aws-chime-web-server/server.js
+++ aws-chime-web-server/server.js
@@ -1,39 +1,44 @@
 'use strict'
 
-const express = require('express');
 const fs = require('fs');
-const https = require('https');
+const compression = require('compression');
+const url = require('url');
 const http = require('http');
-const path = require('path');
-const bodyParser = require('body-parser');
+const app = process.env.npm_config_app || 'meetingV2';
+const indexPagePath = `dist/${app}.html`;
 
-const app = express();
+console.info('Using index path', indexPagePath);
 
-const httpsPort = 443;
-const httpPort = 80;
+const indexPage = fs.readFileSync(indexPagePath);
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+function serve(host = '127.0.0.1:8080') {
+    http.createServer({}, async (request, response) => {
+        try {
+            compression({})(request, response, () => { });
+            const requestUrl = url.parse(request.url, true);
+            if (request.method === 'GET' && requestUrl.pathname === '/') {
+                // Return the contents of the index page
+                respond(response, 200, 'text/html', indexPage);
+            }
+        } catch (error) {
 
-app.use(express.static('./files'));
+        }
+    }).listen(host.split(':')[1], host.split(':')[0], () => {
+        log(`server running at http://${host}/`);
+    });
+}
 
-var httpsOptions = {
-    cert: fs.readFileSync(path.join('/etc/letsencrypt/live/harshmalve.com/cert.pem')),
-    key: fs.readFileSync(path.join('/etc/letsencrypt/live/harshmalve.com/privkey.pem'))
+function respond(response, statusCode, contentType, body, skipLogging = false) {
+    response.statusCode = statusCode;
+    response.setHeader('Content-Type', contentType);
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.end(body);
+    if (contentType === 'application/json' && !skipLogging) {
+        log(body);
+    }
 };
 
-https.createServer(httpsOptions, app).listen(httpsPort, function () {
-    console.log('Magic happens on port ' + httpsPort);
-});
-
-// Redirect from http port to https
-http.createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'].replace(httpPort, httpsPort) + req.url });
-    console.log("http request, will go to >> ");
-    console.log("https://" + req.headers['host'].replace(httpPort, httpsPort) + req.url);
-    res.end();
-}).listen(httpPort);
-
-app.route('/home').get((req, res, next) => {
-    console.log('request ' + req.url);
-    res.status(302).redirect('/')
-  });
+function log(message) {
+    console.log(`${new Date().toISOString()} ${message}`);
+}
+serve();
